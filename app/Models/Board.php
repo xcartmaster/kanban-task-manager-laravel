bc8ce80a73044871e6ca525e0bc46fa0a00db5fe
Michael Evstigneev <xcartmaster@gmail.com>
2026-07-14 16:31:20 +0400

    Model: establish User-Board relationships and implement canCreateBoard business logic

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Board extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'name'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use \Illuminate\Database\Eloquent\Relations\hasMany;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -16,6 +17,23 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    public function boards(): hasMany
+    {
+        return $this->hasMany(Board::class);
+    }
+
+    public function canCreateBoard(): bool
+    {
+        // 1. Проверяем, активна ли подписка прямо сейчас.
+        // Метод isFuture() проверяет, находится ли дата окончания в будущем.
+        if ($this->is_subscribed && $this->subscription_ends_at && $this->subscription_ends_at->isFuture()) {
+            return true;
+        }
+
+        // 2. Если подписки нет или она истекла, проверяем лимит бесплатных досок
+        return $this->boards()->count() < 3;
+    }
 
     /**
      * Get the attributes that should be cast.
@@ -27,6 +45,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_subscribed' => 'boolean',
+            'subscription_ends_at' => 'datetime',
         ];
     }
 }
