b2ec7ce3c5365b551bd6a9c7f6138cb80c1807c1
Michael Evstigneev <xcartmaster@gmail.com>
2026-07-14 14:42:22 +0400

    Migration: add subscription fields to users table

---
+++ database/migrations/2026_07_14_103452_add_subscription_fields_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_subscribed')->default(false)->after('remember_token');
+            $table->timestamp('subscription_ends_at')->nullable()->after('is_subscribed');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['is_subscribed', 'subscription_ends_at']);
+        });
+    }
+};
